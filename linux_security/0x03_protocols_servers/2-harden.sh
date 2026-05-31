080de1740cae815800ae9b0c8e64d0426585c63c
AKNAYKHAN <132621067+AKNAYKHAN@users.noreply.github.com>
2026-05-31 23:05:41 +0400

    Replace Python script with Bash command for hardening

--- linux_security/0x03_protocols_servers/2-harden.sh
+++ linux_security/0x03_protocols_servers/2-harden.sh
@@ -1,22 +1,2 @@
-#!/usr/bin/env python3
- 
-import os
-import stat
- 
- 
-def harden_world_writable_dirs(start_path="/"):
-    for dirpath, dirnames, _ in os.walk(start_path):
-        try:
-            mode = os.stat(dirpath).st_mode
-            if mode & stat.S_IWOTH:
-                print(dirpath)
-                new_mode = mode & ~stat.S_IWOTH
-                os.chmod(dirpath, new_mode)
-        except PermissionError:
-            pass
-        except OSError:
-            pass
- 
- 
-if __name__ == "__main__":
-    harden_world_writable_dirs("/")
+#!/bin/bash
+find / -type d -perm -0002 -exec chmod o-w {} + -print 2>/dev/null
